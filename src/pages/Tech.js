417fc2892ac744fb4e6eaa6b65adae95db697433
Seung-wan <tmddhks0104@gmail.com>
2023-06-20 19:45:14 +0900

    feat: tech page

--- src/pages/Tech.js
+++ src/pages/Tech.js
@@ -1,8 +1,29 @@
+import techs from '../assets/jsons/tech.json' assert { type: 'json' };
+
 export const Tech = () => {
   return (
     /* HTML */
-    ` <div>
-      <h2>Tech</h2>
-    </div>`
+    `
+      <div class="tech">
+        <h2>테크</h2>
+        <ul>
+          ${techs.data.map((tech) => {
+            return (
+              /* HTML */
+              `<li>
+                <div>
+                  <img class="tech__image" src=${tech.imgSrc} alt="logo" />
+                </div>
+                <div>
+                  <div>${tech.title}</div>
+                  <div>${tech.text}</div>
+                  <time datetime=${tech.createdAt}>${tech.createdAt}</time>
+                </div>
+              </li>`
+            );
+          })}
+        </ul>
+      </div>
+    `
   );
 };
